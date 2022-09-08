a0fe42d291842d97bc064561ad9fa47d4cab8083
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-08 16:35:38 -0700

    Scripts Created for ALX C programming tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Solution to advanced task alx low level programming
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	write(2,
+	      "amd that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+	return (1);
+}
