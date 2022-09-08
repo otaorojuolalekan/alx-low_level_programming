b76e11db6e6494c35ea42928e95d369d4cd37b71
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-08 11:10:04 -0700

    Scripts Created for ALX C programming tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-/*
- *uses the puts function
- *to output to stdout
- *
+/**
+ * uses the puts function
+ * to output to stdout
+ * Return success - Always 0
  */
 int main(void)
 {
   puts ("\"Programming is like building a multilingual puzzle'");
-  return 0;
+  return (0);
 }
