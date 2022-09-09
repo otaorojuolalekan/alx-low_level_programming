6038103059c600800389305360ad3b5d0ec305a6
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-09 14:34:38 -0700

    Scripts Created for ALX C programming tasks

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:  prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 48; k <= 57; k++)
+	{
+		putchar(k);
+		if (k < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
