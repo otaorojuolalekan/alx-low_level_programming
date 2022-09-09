78a5a176781d76426e1d09134555765d20861263
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-09 09:45:27 -0700

    Scripts Created for ALX C programming tasks

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,8 +20,8 @@ int main(void)
 	if (last == 0)
 		printf("Last digit of %d is %d and is 0\n", n, last);
 	else if (last > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-	else if (last < 6 && lastDigit != 0)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	else if (last < 6 && last != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	return (0);
 }
