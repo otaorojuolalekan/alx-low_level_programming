dcb236710f269af57ea57cca1a6f62bd6f60afe8
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-09 14:25:25 -0700

    Scripts Created for ALX C programming tasks

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (num = 48; num <= 57; num++)
 		putchar(num);
-	for (num = 97; num <= 122; num++)
+	for (num = 97; num <= 102; num++)
 		putchar(num);
 	putchar('\n');
 	return (0);
