0492d2153310603aa670f3933b364f42b534d38d
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-09 14:53:11 -0700

    Scripts Created for ALX C programming tasks

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description: possible combinations of 2 digits
+ * Return: Always zero(0)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			if (a==b || b <= a)
+			{
+			}
+			else
+			{
+				putchar(a);
+				putchar(b);
+				if (a == 56 && b == 57)
+				{
+				}
+				else
+				{
+					putchar(44);
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
