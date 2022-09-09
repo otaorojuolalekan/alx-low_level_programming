dba8896e53d5d2b559199b0db1be8af971b37703
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-09 14:11:21 -0700

    Scripts Created for ALX C programming tasks

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - 6-print_numberz.c
+ * Description: print numbers 0..9 using putchar
+ * Return: 0123456789
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
