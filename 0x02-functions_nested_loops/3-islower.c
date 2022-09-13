a3a8703562aa562224fd8137565d7ff78e94dbae
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-13 10:38:45 -0700

    Scripts Created for 0x02-functions_nested_loops tasks

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - checks and returns 1 if input is lowercase
+ *@c: input being checked
+ * Description: same as above
+ * Return: int 1 or 0
+ */
+
+int _islower(int c)
+{
+	if (c > 97 && c < 123)
+		return (1);
+	else
+		return (0);
+}
