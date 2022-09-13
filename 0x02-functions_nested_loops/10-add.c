a82b5020a42ab93f44c431a1e3d45ccbee031dd3
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-13 16:11:25 -0700

    Scripts Created for 0x02-functions_nested_loops tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - check the code
+ * Description: Adds x and y
+ * @x: variable x
+ * @y:  variable y
+ * Return: x + y
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
