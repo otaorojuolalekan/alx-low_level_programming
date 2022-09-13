2dcf759ea57cea2b5fa22a1fdcada6094bc61a72
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-13 09:41:57 -0700

    Scripts Created for ALX C programming tasks

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*main - function prints text as output
+*
+*Return:return 0
+*/
+int main(void)
+{
+	int ch;
+	
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar(10);
+		return (0);
+}
