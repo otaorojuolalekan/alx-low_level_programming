cf6346a552e8e17b105eafa424a8f07f519146fe
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-13 14:09:39 -0700

    Scripts Created for 0x02-functions_nested_loops tasks

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * jack_bauer - prints time from 00:00 to 23:59
+ * Description: same as above
+ * Return: int 1 or 0 or -1
+ */
+
+void jack_bauer(void)
+{
+	char i, j, k, l;
+
+	for (i = '0'; i <= '2'; i++)
+	{
+		for (j = '0'; j <= '3'; j++)
+		{
+			for (k = '0'; k <= '5'; k++)
+			{
+				for (l = '0'; l <= '9'; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar(10);
+				}
+			}
+		}
+	}
+}
