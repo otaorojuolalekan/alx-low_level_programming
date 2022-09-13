58e0979652b6ffb874f186e9a106e41dbbae8cca
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-13 13:10:11 -0700

    Scripts Created for 0x02-functions_nested_loops tasks

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+File Edit Options Buffers Tools C Help
+#include "main.h"
+/**
+ * print_last_digit - does what the name says
+ *@num: input being checked
+ * Description: same as above
+ * Return: last digit
+ */
+
+int print_last_digit(int num)
+{
+	return (num % 10);
+}
