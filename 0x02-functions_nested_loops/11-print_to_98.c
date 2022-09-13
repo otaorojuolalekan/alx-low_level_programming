78102b15bf0fead19582a6ca6a8929f265d253a1
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-13 16:52:14 -0700

    Scripts Created for 0x02-functions_nested_loops tasks

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{
 			if (i != n)
-				print (", ");
+				printf(", ");
 			printf("%d", i);
 		}
 	}
@@ -24,7 +24,7 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != n)
-				print (", ");
+				printf(", ");
 			printf("%d", i);
 		}
 	}
