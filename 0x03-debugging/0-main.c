122012f3522259465f1f49c16b39f414c6b9151e
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-14 10:28:15 -0700

    Scripts Created for 0x02-functions_nested_loops tasks

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,11 @@
 #include "main.h"
-
 /**
 * main - tests function that prints if integer is positive or negative
 * @i: input for function positive_or_negative
 * Return: 0 declared by @i
 */
 void positive_or_negative(int i);
+
 int main(void)
 {
 	int i;
