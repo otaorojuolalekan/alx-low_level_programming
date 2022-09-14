58a7ecd7638981aa749289551445e5c2dc6485da
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-13 17:55:34 -0700

    Scripts Created for 0x02-functions_nested_loops tasks

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - fibonacci 50
+ * Description: fibonacci
+ * Return:  (Success)
+ */
+
+int main(void)
+{
+	unsigned long int i;
+	unsigned long int cur = 1;
+	unsigned long int next = 2;
+
+	printf("%lu", cur);
+
+	for (i = 1; i < 50; i++)
+	{
+		printf(", %lu", aft);
+		cur += next;
+		cur = next - cur;
+	}
+	printf("\n");
+	return (0);
+}
