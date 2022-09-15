3ae2e7ba34e2e6160b5576d50f3761c631b1b849
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-15 15:43:23 -0700

    Scripts Created for 0x04-more_functions_nested_loops tasks

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
@@ -35,7 +35,7 @@ int main(void)
 			printf(" ");
 		}
 	}
-	printf("\b");
+	printf("Buzz");
 	printf("\n");
 	return (0);
 }
