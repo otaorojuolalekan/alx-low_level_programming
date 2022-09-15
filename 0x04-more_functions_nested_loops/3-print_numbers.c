92705a0e25f214cc896a83a2a119c35103abafe0
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-15 10:47:50 -0700

    Scripts Created for 0x04-more_functions_nested_loops tasks

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+/**
+ * print_numbers - prints digits 0 through 9
+ * Description - same as above
+ * Return: 0123456789
+ */
+
+void print_numbers(void)
+{
+	char ch;
+
+	for (ch >= '0'; ch <= '9'; ch++)
+		_putchar(ch);
+	_putchar(10);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,5 +6,6 @@
 int _isupper(char);
 int _isdigit(char);
 int mul(int, int);
+void print_numbers(void);
 
 #endif /* MAIN_H */
