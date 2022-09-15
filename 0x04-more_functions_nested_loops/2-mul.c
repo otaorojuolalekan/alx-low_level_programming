6d83ff228454013ba3c6dd4e91a8e05ffdc1e7b9
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-15 10:39:40 -0700

    Scripts Created for 0x04-more_functions_nested_loops tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - multiplies two number input
+ * Description - same as above
+ * @num1: first input
+ * @num2: second input
+ * Return: num1 * num2
+ */
+
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
