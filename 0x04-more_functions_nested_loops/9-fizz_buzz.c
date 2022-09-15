7e6f14baec85047b6a19f40546e38939cc53e799
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-15 15:29:05 -0700

    Scripts Created for 0x04-more_functions_nested_loops tasks

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz-Buzz program for 1 to 100
+ * Description - for multiples of three print Fizz instead
+ * of the number and for the multiples of five print Buzz.
+ * For numbers which are multiples of both three and five print FizzBuzz.
+ * Return: number or Fizz or Buzz or FizzBuzz depending on condition
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
