53269252868779fe467a304a9124853c22d00ed9
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-15 17:23:32 -0700

    Scripts Created for 0x04-more_functions_nested_loops tasks

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,12 +8,18 @@
 
 void print_number(int n)
 {
+	unsigned int n1;
+	
 	if (n < 0)
 	{
-		n = -n;
+		n1 = -n;
 		_putchar('-');
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	else
+	{
+		n1 = n;
+	}
+	if (n1 / 10)
+		print_number(n1 / 10);
+	_putchar((n1 % 10) + '0');
 }
