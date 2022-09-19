e9dd9499f2b9a8d8d450e05980fa6f0da2c3fb27
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-19 15:22:40 -0700

    Added Scripts for 0x05. C - Pointers, arrays and string tasks

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,6 +10,8 @@ void _puts(char *str);
 void _putchar(char str);
 void print_rev(char *s);
 void puts2(char *str);
+void puts_half(char *str);
 void rev_string(char *s);
+void print_array(int *a, int n);
 
 #endif /* MAIN_H */
