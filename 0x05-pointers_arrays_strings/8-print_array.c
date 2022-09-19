2c5b97a0a8fa5de134047bea71b6ef70f592ee29
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-19 15:15:53 -0700

    Added Scripts for 0x05. C - Pointers, arrays and string tasks

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - prints the content of array- comma separated
+ * Description - same as above
+ * @a: name of array
+ * @n: size of array given
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+			printf("%d", a[i]);
+	}
+}
