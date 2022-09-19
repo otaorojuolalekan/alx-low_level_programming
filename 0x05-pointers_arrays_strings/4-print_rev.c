a07abb5f948787b8b433229154cf1559826406d4
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-19 14:01:27 -0700

    Added Scripts for 0x05. C - Pointers, arrays and string tasks

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints a string of input in reverse
+ * Description - same as above
+ * @s: string input to be printed in reverse
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int count = _strlen(*s);
+
+	while (count > 0)
+	{
+		s--;
+		_putchar(*s);
+		count--;
+	}
+	_putchar(10);
+}
