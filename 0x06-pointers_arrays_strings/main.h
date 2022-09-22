3db5926e5277dd95cc0deedcb990b9b8c657ffdd
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-22 14:39:37 -0700

    Added all prototypes in main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,11 @@ void stringManipulation(char *s);
 int getStringLength(char *str);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
 
 #endif /* MAIN_H */
