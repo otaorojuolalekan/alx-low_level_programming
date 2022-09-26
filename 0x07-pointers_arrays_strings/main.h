ecdde969d84310798e423a5d38db33c3895d4d7f
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-26 11:07:28 -0700

    Added Scripts for 0x06. More Pointers, arrays and string tasks

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@
 int _putchar(char);
 
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 void stringManipulation(char *s);
 int getStringLength(char *str);
 char *_strcat(char *dest, char *src);
