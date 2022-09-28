effae06075ed58edc50a0ff97784e4eb961ade02
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-28 15:32:03 -0700

    Added Scripts for 0x08. C - Recursion tasks

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,13 +3,13 @@
 /**
  * _puts_recursion - prints a string followed by a new line.
  *
- * @s: input character
+ * @s: input string to print
  * Return: Always void
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s == "/0")
+	if (*s == "\0")
 	{
 		_putchar("\n");
 		return;
