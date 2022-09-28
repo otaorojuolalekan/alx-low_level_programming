ad2752e7adfb02b7bd66219832e3dd26e8d09f1f
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-28 16:33:15 -0700

    Added Scripts for 0x08. C - Recursion tasks

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: base input
+ * @y: power input
+ * Return: x^y if y>0 else -1
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (x == 0 || y == 0)
+		return (1);
+	result = x * _pow_recursion(x, y - 1);
+	return (result);
+	}
+}
