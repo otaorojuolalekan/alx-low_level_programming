0f5fc408584dc76d617020a62bf5722622279d20
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-28 16:39:17 -0700

    Added Scripts for 0x08. C - Recursion tasks

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,7 +15,6 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	else if (x == 0 || y == 0)
 		return (1);
-	result = x * _pow_recursion(x, y - 1);
+	result = x * _pow_recursion(x, (y - 1));
 	return (result);
-	}
 }
