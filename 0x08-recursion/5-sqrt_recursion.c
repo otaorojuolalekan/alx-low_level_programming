0b8fc85f8ed0a4631821c4452d05ea8f696f54b8
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-28 17:47:35 -0700

    Added Scripts for 0x08. C - Recursion tasks

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * find_sqrt - helper function to get natural square root recursively
+ * @num: input whose sqrt is checked
+ * @q: incremental value
+ * Return: root(n) or -1
+ */
+
+int find_sqrt(int num, int q)
+{
+	q = 1;
+	if (q * q == num)
+		return (q);
+	if (q * q > num)
+		return (-1);
+	return (find_sqrt(num, q + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: input given
+ * Return: root(n) or -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	int check = find_sqrt(n);
+
+	if (check != -1)
+		return (check);
+	else
+		return (-1);
+}
