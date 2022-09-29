5ad953b156a8f4f34c3752d47775214a525a9eb1
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-28 19:20:41 -0700

    Added Scripts for 0x08. C - Recursion tasks

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/* create a helper function to confirm divisibility */
+
+/**
+ * is_divisible - returns the prime number
+ * @num: input
+ * @iter: recursion iterable
+ * Return: value if prime, else 0
+ */
+
+int is_divisible(num, iter)
+{
+	if (num == 1)
+		return (0);
+	if (num != iter)
+	{
+		if (num % iter == 0)
+			return (0);
+		return (is_divisible(num, iter + 1));
+	}
+	return (num);
+
+/**
+ * is_prime_number -returns 1 if is prime or else 0
+ * @n: input required to check
+ * Return: 0 or 1
+ */
+
+int is_prime_number(int n)
+{
+	if (n == is_divisible(n, 1))
+		return (1);
+	return (0);
+}
