866e95715555ef4cb84c59fa94f29aa8a84e1cf9
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-30 11:05:02 -0700

    Added Scripts for 0x0A. C - argc, argv tasks

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,14 +12,14 @@ int main(int argc, char **argv)
 	int argv1 = atoi(argv[1]);
 	int argv2 = atoi(argv[2]);
 	int mul = argv1 * argv2;
-	if (argc == 3)
-	{
-		printf("%d\n", mul);
-		return (0);
-	}
-	else
+	if (argc == 1 || argc == 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	else
+	{
+		printf("%d\n", mul);
+	}
+	return (0);
 }
