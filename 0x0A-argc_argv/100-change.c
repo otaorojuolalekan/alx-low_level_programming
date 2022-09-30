5a4439b510dd65e804918b6c6dff18175824ac6a
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-30 15:53:12 -0700

    Added Scripts for 0x0A. C - argc, argv tasks

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,34 +7,44 @@
  * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i, j, length, sum;
-	char *ptr;
+	int cents, coins = 0;
 
-	if (argc < 2)
-	printf("0\n");
-	else
+	if (argc != 2)
 	{
-		sum = 0;
-		for (i = 1; i < argc; i++)
-		{
-			ptr = argv[i];
-			length = strlen(ptr);
-
-			for (j = 0; j < length; j++)
-			{
-				if (isdigit(*(ptr + j)) == 0)
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-
-			sum += atoi(argv[i]);
-		}
-
-		printf("%d\n", sum);
+		printf("Error\n");
+		return (1);
 	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
+	}
+
+	printf("%d\n", coins);
 	return (0);
 }
