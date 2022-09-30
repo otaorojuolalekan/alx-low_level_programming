ac26fbbbb09f4dccc8fa7fe1629a4bd70eb1ef6e
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-09-30 07:25:47 -0700

    Added Scripts for 0x0A. C - argc, argv tasks

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: Arguments count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	if (argv)
+	{
+	}
+	printf("%d\n", argc - 1);
+	return (0);
+}
