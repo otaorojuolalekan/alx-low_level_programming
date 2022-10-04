e4fe08daf98da12c5dee1b589cc42a46f68faa37
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-10-04 13:37:25 -0700

    Added Scripts for 0x0B. C - Malloc, Free tasks

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -13,5 +13,6 @@ int atoi(const char *str);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 
 #endif /* MAIN_H */
