5f64cfe5865570dde44436bddd7406d17ebe052b
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-10-10 13:35:49 -0700

    Added Scripts for 0x0E. C - Structures, typedef Tasks

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct for dog
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
