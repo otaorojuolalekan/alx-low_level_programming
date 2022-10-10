e5a0344dd80f38fc8245ab82c94cdba371b40e17
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-10-10 04:43:44 -0700

    Added Scripts for 0x0D. C - Preprocessor Tasks

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point for program
  * Return: void but prints The filename of the program being compiled
@@ -9,5 +8,5 @@
 int main(void)
 {
 	printf("%s\n", filename);
-	return(0);
+	return (0);
 }
