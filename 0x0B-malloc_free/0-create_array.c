d761c0bafd077bd7797feec0c875c3d3e9c63734
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-11-02 11:53:59 +0100

    Added Scripts for 0x0B. C - Malloc, Free tasks

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 /**
- * create_array - Creates an array of chars and
- *                initializes it with a specific char.
- * @size: The size of the array to be initialized.
- * @c: The specific char to intialize the array with.
- *
- * Return: If size == 0 or the function fails - NULL.
- *         Otherwise - a pointer to the array.
+ * create_array - function that creates an array of chars, ...
+ * ... and initializes it with a specific char.
+ * @size: Size of array
+ * @c: character to be initialized
+ * Return: A pointer to the function.
  */
+
+
 char *create_array(unsigned int size, char c)
 {
 	char *array;
-	unsigned int index;
+	unsigned int i;
+
+	array = malloc(sizeof(array) * size);
 
 	if (size == 0)
 		return (NULL);
-
-	array = malloc(sizeof(char) * size);
-
 	if (array == NULL)
 		return (NULL);
-
-	for (index = 0; index < size; index++)
-		array[index] = c;
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
 
 	return (array);
 }
