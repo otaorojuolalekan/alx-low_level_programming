964298bf804e2531588042c26bc6606509851081
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-11-05 08:59:57 +0100

    Added Scripts for 0x0B. C - Malloc, Free tasks

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,13 +16,17 @@ int **alloc_grid(int width, int height)
 	if (width == 0 || height == 0)
 		return (NULL);
 	if (arr == NULL)
+	{
+		free(arr);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(width * sizeof(int));
 		if (arr[i] == NULL)
 		{
+			free(arr);
 			return (NULL);
 		}
 	}
